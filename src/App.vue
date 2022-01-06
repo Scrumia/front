6f6644ada6647e90163d89fc75487400386d228e
medi.demirdelen <medi.demirdelen@gmail.com>
2022-01-06 14:44:11 +0100

    remove material

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <template>
-<!--
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
--->
-  <Requetes />
+  <Requetes></Requetes>
 </template>
 
 <script>
-import Requetes from "./components/Requetes.vue";
-
+import Requetes from "./components/Requetes";
 export default {
   name: "App",
   components: {
@@ -18,12 +13,10 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #F6F2ED;
 }
 </style>
--- src/components/Requetes.vue
+++ src/components/Requetes.vue
@@ -1,18 +1,63 @@
 <template>
-    <div class="md-layout">
-        <div class="md-layout-item md-medium-size-33 md-small-size-50 md-xsmall-size-100">
-            <RequeteCard :requestName="'test'" :requestDate="'date'"></RequeteCard>
-        </div>
+    <div>
+        <ul class="liste">
+            <li v-for="quest in dataRequetes" :key="quest.title">
+                <div class="requete">
+                    <div class="titreRequete">
+                        {{quest.title}}
+                    </div>
+                    <div class="dateRequete">
+                        {{quest.date}}
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-import RequeteCard from './RequeteCard.vue';
-
+import {Quest} from "@/model/quest.model";
 export default {
     name: "Requetes",
-    components: {
-        RequeteCard,
+    data() {
+        return {
+            dataRequetes: [
+                new Quest('Titre requête n°1', '01/01/01'),
+                new Quest('Titre requête n°2', '02/02/02'),
+                new Quest('Titre requête n°3', '03/03/03'),
+            ]
+        }
     }
 }
 </script>
+
+<style scoped>
+li {
+    list-style: none;
+    height: 10em;
+    width: 7.5em;
+    margin: 1.5em;
+}
+.requete {
+    background-color: white;
+    border-radius: 12px 12px 0px 0px;
+    border: none;
+    box-shadow: 3px 5px 5px #b5b5b5;
+    height: 80%;
+    width: 100%;
+    display: flex;
+    z-index: 0;
+    position: relative;
+}
+.titreRequete {
+    font-weight: bold;
+}
+.dateRequete {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+}
+.liste {
+    display: flex;
+}
+</style>
