ed28fc44b9afa59b4c664ec89260cfa026bfe30c
xenon-cripton <brhulla@gmail.com>
2022-01-13 11:25:31 +0100

    nouvelle correction du fichier deploy

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
@@ -17,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Scrumia/Scrumia.github.io.git master:gh-pages
+git push -f git@github.com:xenon-cripton/xenon-cripton.github.io.git main:gh-pages
 
 cd -
