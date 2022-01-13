b2cff3b0161f7756464bd264322f50cd3c49a0e9
xenon-cripton <brhulla@gmail.com>
2022-01-13 11:50:44 +0100

    nouvelle correction du fichier deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:/Scrumia/front.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:xenon-cripton/Scrumia/front.git master:gh-pages
+
 
 cd -
