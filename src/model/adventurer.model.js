b578e2ca864584fd98b00be9a6ba58a86f987099
xenon-cripton <brhulla@gmail.com>
2022-01-21 14:50:36 +0100

    us115 ajout nom utilisateur dans le header

--- src/model/adventurer.model.js
+++ src/model/adventurer.model.js
@@ -1,13 +1,13 @@
 export class Adventurer {
-  full_name;
-  experience_level;
-  status;
-  speciality;
+	full_name;
+	experience_level;
+	status;
+	speciality;
 
-  constructor(full_name, experience_level, status, speciality) {
-    this.full_name = full_name;
-    this.experience_level = experience_level;
-    this.status = status;
-    this.speciality = speciality;
-  }
+	constructor(full_name, experience_level, status, speciality) {
+		this.full_name = full_name;
+		this.experience_level = experience_level;
+		this.status = status;
+		this.speciality = speciality;
+	}
 }
